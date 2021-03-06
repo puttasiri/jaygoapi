441b8bfe9b2566c13a8e1fe994901db6aeecda2f
Puttasiri (IT.Appl.ADM) <puttasiri@corp.jaymart.co.th>
2021-03-06 14:02:56 +0700

    initialize go project

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/puttasiri/jaygoapi
+
+go 1.16
+
+require github.com/labstack/echo/v4 v4.2.0
---
+++ server.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+func helloHandler(c echo.Context) error {
+	return c.JSON(http.StatusOK, map[string]string{
+		"message": "hello",
+	})
+}
+func main() {
+	e := echo.New()
+	e.GET("/hello", helloHandler)
+	e.Start(":1323")
+}
