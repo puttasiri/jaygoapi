b4301235a5b63f2739013337d5682f160acabfd4
Puttasiri (IT.Appl.ADM) <puttasiri@corp.jaymart.co.th>
2021-03-13 14:31:14 +0700

    commit todo select from db

--- server.go
+++ server.go
@@ -5,117 +5,45 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/puttasiri/jaygoapi/todo"
+
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
-type Todo struct {
-	ID     int    `json:"id"` //ขึ้นต้นด้วยตัวพิมพ์ใหญ่เท่านั้น
-	Title  string `json:"title"`
-	Status string `json:"status"`
-}
-
-var todos = map[int]*Todo{
-	1: &Todo{ID: 1, Title: "pay phone bills", Status: "Active"},
-}
-
-func helloHandler(c echo.Context) error {
-	return c.JSON(http.StatusOK, map[string]string{
-		"message": "hello puttasiri",
-	})
-}
-
-func getTodosHandler(c echo.Context) error { //เปลี่ยน data เป็น ่json
-	items := []*Todo{}
-	for _, item := range todos {
-		items = append(items, item)
+func authMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		log.Println("start middleware : check authentication")
+		token := c.Request().Header.Get("Authorization")
+		if token != "ABC" {
+			return c.JSON(http.StatusUnauthorized, map[string]string{"message": "Unauthorized"})
+		}
+		return next(c)
 	}
-	return c.JSON(http.StatusOK, items)
 }
 
-func createTodosHandler(e echo.Context) error {
-	t := Todo{}
-	if err := e.Bind(&t); err != nil {
-		return e.JSON(http.StatusBadRequest, map[string]string{"error": err.Error()})
-	}
-
-	id := len(todos)
-	id++
-	t.ID = id
-	todos[t.ID] = &t
-	return e.JSON(http.StatusCreated, "created todo.")
-}
-
-func getTodoByIdHandler(c echo.Context) error {
-	var id int
-	err := echo.PathParamsBinder(c).Int("id", &id).BindError()
-	if err != nil {
-		return c.JSON(http.StatusBadRequest, err)
-	}
-
-	t, ok := todos[id]
-	if !ok {
-		return c.JSON(http.StatusOK, map[int]string{})
-	}
-	return c.JSON(http.StatusOK, t)
-}
-func updateTodosHandler(c echo.Context) error {
-	var id int
-	err := echo.PathParamsBinder(c).Int("id", &id).BindError()
-	if err != nil {
-		return c.JSON(http.StatusBadRequest, err)
-	}
-	t := todos[id]
-	if err := c.Bind(t); err != nil {
-		return c.JSON(http.StatusBadRequest, map[string]string{"error": err.Error()})
-	}
-	return c.JSON(http.StatusOK, t)
-	// var id int
-	// err := echo.PathParamsBinder(e).Int("id", &id).BindError()
-	// if err != nil {
-	// 	return e.JSON(http.StatusBadRequest, err)
-	// }
-
-	// t := Todo{}
-	// if err := e.Bind(&t); err != nil {
-	// 	return e.JSON(http.StatusBadRequest, map[string]string{"error": err.Error()})
-	// }
-
-	// id := len(todos)
-	// id++
-	// t.ID = id
-	// todos[t.ID] = &t
-	// return c.JSON(http.StatusOK, "updated todo.")
-}
-
-func deleteTodosHandler(c echo.Context) error {
-	var id int
-	err := echo.PathParamsBinder(c).Int("id", &id).BindError()
-	if err != nil {
-		return c.JSON(http.StatusBadRequest, err)
-	}
-	delete(todos, id)
-	return c.JSON(http.StatusOK, "deleted todo.")
-}
 func main() {
 	e := echo.New()
 
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
-	e.GET("/hello", helloHandler)
+	e.Use(authMiddleware)
+
+	e.GET("/hello", todo.HelloHandler)
 	//e.Start(":1323")
 
-	e.GET("/todos", getTodosHandler)
-
-	e.POST("/todos", createTodosHandler)
+	e.GET("/todos", todo.GetTodosHandler)
 
 	//insert
-	e.GET("/todos/:id", getTodoByIdHandler)
+	e.POST("/todos", todo.CreateTodosHandler)
+
+	//select by id
+	e.GET("/todos/:id", todo.GetTodoByIdHandler2)
 
 	//update PUT
-	e.PUT("/todos/:id", updateTodosHandler)
+	e.PUT("/todos/:id", todo.UpdateTodosHandler)
 	//delete DELETE
-	e.DELETE("/todos/:id", deleteTodosHandler)
+	e.DELETE("/todos/:id", todo.DeleteTodosHandler)
 
 	port := os.Getenv("PORT")
 	log.Println("Port:", port)
---
+++ todo/todo.go
@@ -0,0 +1,137 @@
+package todo
+
+import (
+	"database/sql"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/labstack/echo/v4"
+)
+
+type Todo struct {
+	ID     int    `json:"id"` //ขึ้นต้นด้วยตัวพิมพ์ใหญ่เท่านั้น
+	Title  string `json:"title"`
+	Status string `json:"status"`
+}
+
+var todos = map[int]*Todo{
+	1: &Todo{ID: 1, Title: "pay phone bills", Status: "Active"},
+}
+
+func HelloHandler(c echo.Context) error {
+	return c.JSON(http.StatusOK, map[string]string{
+		"message": "hello puttasiri",
+	})
+}
+
+func GetTodosHandler(c echo.Context) error { //เปลี่ยน data เป็น ่json
+	items := []*Todo{}
+	for _, item := range todos {
+		items = append(items, item)
+	}
+	return c.JSON(http.StatusOK, items)
+}
+
+func CreateTodosHandler(e echo.Context) error {
+	t := Todo{}
+	if err := e.Bind(&t); err != nil {
+		return e.JSON(http.StatusBadRequest, map[string]string{"error": err.Error()})
+	}
+
+	id := len(todos)
+	id++
+	t.ID = id
+	todos[t.ID] = &t
+	return e.JSON(http.StatusCreated, "created todo.")
+}
+
+func GetTodoByIdHandler(c echo.Context) error {
+	var id int
+	err := echo.PathParamsBinder(c).Int("id", &id).BindError()
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, err)
+	}
+
+	t, ok := todos[id]
+	if !ok {
+		return c.JSON(http.StatusOK, map[int]string{})
+	}
+	return c.JSON(http.StatusOK, t)
+}
+func UpdateTodosHandler(c echo.Context) error {
+	var id int
+	err := echo.PathParamsBinder(c).Int("id", &id).BindError()
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, err)
+	}
+	t := todos[id]
+	if err := c.Bind(t); err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]string{"error": err.Error()})
+	}
+	return c.JSON(http.StatusOK, t)
+	// var id int
+	// err := echo.PathParamsBinder(e).Int("id", &id).BindError()
+	// if err != nil {
+	// 	return e.JSON(http.StatusBadRequest, err)
+	// }
+
+	// t := Todo{}
+	// if err := e.Bind(&t); err != nil {
+	// 	return e.JSON(http.StatusBadRequest, map[string]string{"error": err.Error()})
+	// }
+
+	// id := len(todos)
+	// id++
+	// t.ID = id
+	// todos[t.ID] = &t
+	// return c.JSON(http.StatusOK, "updated todo.")
+}
+
+func DeleteTodosHandler(c echo.Context) error {
+	var id int
+	err := echo.PathParamsBinder(c).Int("id", &id).BindError()
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, err)
+	}
+	delete(todos, id)
+	return c.JSON(http.StatusOK, "deleted todo.")
+}
+
+func GetTodoByIdHandler2(c echo.Context) error {
+
+	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Fatal("Connect to database error", err)
+	}
+	defer db.Close()
+
+	stmt, err := db.Prepare("SELECT id, title, status FROM todos where id=$1") //select more row
+	if err != nil {
+		log.Fatal("can'tprepare query one row statment", err)
+	}
+
+	rowId := 1
+	row := stmt.QueryRow(rowId)
+	var id int
+	var title, status string
+
+	err = row.Scan(&id, &title, &status)
+	if err != nil {
+		log.Fatal("can't Scan row into variables", err)
+	}
+
+	//var id int
+	err = echo.PathParamsBinder(c).Int("id", &id).BindError()
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, err)
+	}
+
+	t, ok := todos[id]
+	if !ok {
+		return c.JSON(http.StatusOK, map[int]string{})
+	}
+	return c.JSON(http.StatusOK, t)
+
+	//fmt.Println("one row", id, title, status)
+}
