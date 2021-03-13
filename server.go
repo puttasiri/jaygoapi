1d8b08d1961bad768c5099bbe8145c68036136ae
Puttasiri (IT.Appl.ADM) <puttasiri@corp.jaymart.co.th>
2021-03-13 15:11:31 +0700

    commit todo select from db v3

--- server.go
+++ server.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"log"
-	"net/http"
 	"os"
 
 	"github.com/puttasiri/jaygoapi/todo"
@@ -11,23 +10,23 @@ import (
 	"github.com/labstack/echo/v4/middleware"
 )
 
-func authMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
-	return func(c echo.Context) error {
-		log.Println("start middleware : check authentication")
-		token := c.Request().Header.Get("Authorization")
-		if token != "ABC" {
-			return c.JSON(http.StatusUnauthorized, map[string]string{"message": "Unauthorized"})
-		}
-		return next(c)
-	}
-}
+// func authMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
+// 	return func(c echo.Context) error {
+// 		log.Println("start middleware : check authentication")
+// 		token := c.Request().Header.Get("Authorization")
+// 		if token != "ABC" {
+// 			return c.JSON(http.StatusUnauthorized, map[string]string{"message": "Unauthorized"})
+// 		}
+// 		return next(c)
+// 	}
+// }
 
 func main() {
 	e := echo.New()
 
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
-	e.Use(authMiddleware)
+	// e.Use(authMiddleware)
 
 	e.GET("/hello", todo.HelloHandler)
 	//e.Start(":1323")
